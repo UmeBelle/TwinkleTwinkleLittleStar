5d3d993f7c2923d766850a21fc97feb08d87d84a
Nicolas <nicolaspan.op@gmail.com>
2022-10-01 14:56:40 -0300

    primeros pasos backend

--- backend/comoquieras.py
+++ backend/comoquieras.py
@@ -1,9 +1,25 @@
-#Dado un nombre y una fecha de nacimiento codificamos (b,l)
+#Dado un nombre y una fecha de nacimiento codificamos unas coordenadas 
+#galacticas (b,l)
+import numpy as np
+import math
 
-a = string('NicolasPanRivero')
-print(len(a))
+n = "NicolasPanRivero"
+# print(len(a))
 
+d = 20
+m = 3
+a = 2000
 
-#Dadas unas coordenadas galácticas (b,l) buscamos 
-#mirar en esa direccion con un catálogo de estrellas variables
+def toBinary(cosa):
+  l,m=[],[]
+  for i in cosa:
+    l.append(ord(i))
+  for i in l:
+    m.append(int(bin(i)[2:]))
+  return m
+
+print(n,"en binario es") 
+print(toBinary("n"))
+
+b = 20+3+2000
 
