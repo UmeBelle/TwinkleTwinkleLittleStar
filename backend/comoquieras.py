8668e958081b8003a05c7324e6a652015161da36
Nicolas <nicolaspan.op@gmail.com>
2022-10-02 13:39:07 -0300

    Agregue el pedido del sentimiento para transformarlo a binario y asi elegir una de la lista de estrellas

--- backend/comoquieras.py
+++ backend/comoquieras.py
@@ -3,23 +3,64 @@
 import numpy as np
 import math
 
-n = "NicolasPanRivero"
-# print(len(a))
+d = int(input('dia'))
+m = int(input('mes'))
 
-d = 20
-m = 3
-a = 2000
+#d = 20
+#m = 3
 
-def toBinary(cosa):
-  l,m=[],[]
-  for i in cosa:
-    l.append(ord(i))
-  for i in l:
-    m.append(int(bin(i)[2:]))
-  return m
+fecha = m+d
+#Para mapear un numero x de un intervalo [a,b] a un intervalo [c,d] hacemos:
+def b(fecha):
+    a=2
+    b=43
+    c=0
+    d=90
+    resu = c + (((d-c)/(b-a))*(fecha-a))
+    return resu
+print("Su latitud galáctica es b=",b(fecha))
 
-print(n,"en binario es") 
-print(toBinary("n"))
+def l(fecha):
+    a=2
+    b=43
+    c=0
+    d=360
+    resu = c + (((d-c)/(b-a))*(fecha-a))
+    return resu
+print("Su longitud galáctica es l=",l(fecha))
 
-b = 20+3+2000
+#--------------------------------
 
+n = input('¿Cómo lo hace sentir esta imagen?')
+#Transformamos el string en un binario
+#test_str = "Pan"  
+res = int(''.join(format(ord(i), '08b') for i in n)) 
+#Se pierden los ceros iniciales pero da igual
+#print(res)
+
+#--------------------------------
+
+#Obtenemos la suma de todos los 1s del binario
+my_list = []
+for x in str(res):
+    my_list.append(int(x))
+#print(my_list) # 👉️ [1, 3, 5, 7, 9]
+
+numero = np.random.normal(loc=sum(my_list), scale=fecha, size=None)
+
+print("Su estrella es la numero",int(abs(numero))+1)
+
+#--------------------------------
+
+#Luego que tenemos las coordenadas galácticas buscamos en algun catálogo 
+#como Gaia y obtenemos una imagen de campo de la cual seleccionamos 
+#la correspondiente a la palabra que escribio la persona
+
+#Cuando tenemos ese campo hay que hacer un crossmatch con algun catalogo de
+#variabilidad (¡EN EL FUTURO CERCANO VERA RUBIN!) para saber si alguna de los 
+#objetos del campo es alguno de las cosas que estamos estudiando: Supernovas,
+#binarias, planetas transitantes o estrellas pulsantes.
+#Al usuario se le aparece la estrella correspondiente a la palabra que eligio
+#y se muestra el tipo de variabilidad que es con una animación clickeable.
+#Luego aparece el boton de "leer mas" para que aparezca la informacion mas
+#tecnica.
